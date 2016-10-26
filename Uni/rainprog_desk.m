de74660d3f7103752f665a231bb13ef8d15e8c9e
Joscha Fregin <joscha.fregin@googlemail.com>
2016-10-26 18:56:58 +0200

    Add files via upload

--- Uni/rainprog_desk.m
+++ Uni/rainprog_desk.m
@@ -1,10 +1,10 @@
 close all;clear;
 
-filepath='C:/Users/Uni/Desktop/m4t_BKM_wrx00_l2_dbz_v00_20130426190000.nc';
+filepath='D:/Programmieren/Rain/m4t_BKM_wrx00_l2_dbz_v00_20130511160000.nc';
 data=ncread(filepath,'dbz_ac1');
 azi=ncread(filepath,'azi');
 range=ncread(filepath,'range');
-
+ 
 % 
 % for i=1:size(data,3)
 %     figure
@@ -29,9 +29,6 @@ for theta=1:360
     for r=1:333     
         x(r,theta)=range(r)*cos(azi(theta)*pi/180);
         y(r,theta)=range(r)*sin(azi(theta)*pi/180);
-        for i=1:120
-            dataRain(r,theta,i)=0.0364633*(10^(data(r,theta,i)/10))^0.625;
-        end
     end
 end
 
@@ -103,23 +100,33 @@ max_=max(max(max(dataRain(:,:,:))));
 % set(h,'LineColor','none')
 %%%%%%%%%%%%%%%%%%%%
 
+%cartesian coordinatesystem
 x_car = -20000:100:20000;
 y_car = -20000:100:20000;
 [X,Y]= meshgrid(x_car,y_car);
-timesteps=50;
+
+
+
+timesteps=120;
+c_range=30;
+d_s=length(X);
+
+%preallocating
 data_car=cell(timesteps,1);
 maxima=cell(timesteps,1);
 c_max=cell(timesteps,1);
+nested_data= zeros(d_s+2*c_range,d_s+2*c_range,timesteps);
 
+%transformation from polar to cartesian
 for i=1:timesteps
     tic
+    data(:,:,i)=0.0364633*(10.^(data(:,:,i)/10)).^0.625;
     data_car{i}= griddata(x,y,data(:,:,i),X,Y);
+    %data_car{i}=0.0364633*(10^(data_car{i}/10))^0.625;
     toc
-    [val, max_x]=max(max(data_car{i}(:,:)));
-	[val, max_y]=max(data_car{i}(:,max_x));
-    maxima{i}(1)=max_x;
-    maxima{i}(2)=max_y;
+
 end
+    %setting nans to 0
 for i=1:timesteps
     for j=1:length(data_car{i})
         for o=1:length(data_car{i})
@@ -128,42 +135,47 @@ for i=1:timesteps
             end
         end
     end
+
+    nested_data(c_range+1:c_range+d_s,c_range+1:c_range+d_s,i)= data_car{i};
+    [val, max_y]=max(max(nested_data(:,:,i)));
+	[val, max_x]=max(nested_data(:,max_y,i));
+    maxima{i}(1)=max_x;
+    maxima{i}(2)=max_y;
+    maxima{i}(3)=mean([nested_data(max_x,max_y,i),nested_data(max_x+1,max_y,i),nested_data(max_x+1,max_y+1,i),nested_data(max_x-1,max_y,i),nested_data(max_x-1,max_y-1,i)]);
 end
-c_range=30;
-c_range_bottom=c_range;
-c_range_top=c_range;
-c_range_right=c_range;
-c_range_left=c_range;
-c_max{1}=maxima{1};
-dim=length(data_car{1});
+
+%nesting the array into a bigger array
+
+
+
+
+c_max{1}=maxima{1}(2:-1:1);
+o=0;
+Contours=[0.1 0.2 0.5 1 2 5 10 100];
 
 for i=1:timesteps-1
-    %corr_area=(data_car{i}((c_max{i}(2)-c_range):(c_max{i}(2)+c_range),(c_max{i}(1)-c_range):(c_max{i}(1)+c_range)));
-    if c_max{i}(1)+c_range > dim
-        c_range_right = c_range-(c_max{i}(1)+c_range-dim);
+    if nested_data(c_max{i}(1),c_max{i}(2),i) - mean([nested_data(c_max{i}(1),c_max{i}(2),i),nested_data(c_max{i}(1)+1,c_max{i}(2),i),nested_data(c_max{i}(1)+1,c_max{i}(2)+1,i),nested_data(c_max{i}(1)-1,c_max{i}(2),i),nested_data(c_max{i}(1)-1,c_max{i}(2)-1,i)]) > 0.1 %& nested_data(c_max{i}(1),c_max{i}(2),i) == 0
+        c_max{i}=maxima{i}(2:-1:1);
+        warning('chose new maximum')
+        o=o+1;
     end
-    if c_max{i}(2)+c_range> dim
-        c_range_top = c_range-(c_max{i}(2)+c_range-dim);
-    end
-    if c_max{i}(1)-c_range<1
-        c_range_left = c_range-abs(c_max{i}(1)-c_range);
-    end
-    if c_max{i}(2)-c_range<1
-        c_range_bottom = c_range-abs(c_max{i}(2)-c_range);
-    end
-    corr_area=(data_car{i}((c_max{i}(2)-c_range_bottom):(c_max{i}(2)+c_range_top),(c_max{i}(1)-c_range_left):(c_max{i}(1)+c_range_right)));
-    
-    size(corr_area)
-    C=xcorr2(data_car{i+1},corr_area);
+    corr_area=nested_data((c_max{i}(2)-c_range):(c_max{i}(2)+c_range),(c_max{i}(1)-c_range):(c_max{i}(1)+c_range),i);
+    C=xcorr2(nested_data(:,:,i+1),corr_area);
     [ssr,snd]=max(C(:));
     [y_,x_]=ind2sub(size(C),snd);
-    c_max{i+1}(1)=x_-c_range_right;
-    c_max{i+1}(2)=y_-c_range_top;
-    contourf(data_car{i})
+    c_max{i+1}(1)=x_-c_range;
+    c_max{i+1}(2)=y_-c_range;
+    contourf(log(nested_data(:,:,i)),log(Contours))
+    colorbar('YTick',log(Contours),'YTickLabel',Contours);
+    colormap(jet);
+    caxis(log([Contours(1) Contours(length(Contours))]));
+    colorbar('FontSize',12,'YTick',log(Contours),'YTickLabel',Contours);
     hold on
-    plot(c_max{i}(1),c_max{i}(2),'m*','MarkerSize',20)
-    plot(c_max{i+1}(1),c_max{i+1}(2),'b*','MarkerSize',20)
-    pause(2)
+    plot(c_max{i}(1),c_max{i}(2),'go','MarkerSize',20,'MarkerFaceColor','g')
+    plot(c_max{i+1}(1),c_max{i+1}(2),'bo','MarkerSize',20,'MarkerFaceColor','b')
+    line([c_max{i+1}(1) c_max{i}(1) ],[c_max{i+1}(2) c_max{i}(2)],'LineWidth',5,'Color','k')
+    %plot(maxima{i}(2),maxima{i}(1),'ko','MarkerSize',20,'MarkerFaceColor','k')
+    pause(0.5)
     hold off
-end
 
+end
