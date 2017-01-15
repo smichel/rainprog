79161588299b2c8330a67de9e88cb42d14959977
smichel <simon.michel.j@gmail.com>
2017-01-16 00:21:12 +0100

    code for plotting
    implemented the code to plot the correlation with different parameters,
    e.g. resolution

--- matlab/rainprog_corr.m
+++ matlab/rainprog_corr.m
@@ -1,23 +1,26 @@
 close all; clear;
-parfor i=1:1:10
-    for res=1:2
+for i=1:1:10
+    for res=100:100:200
     prog=(i+2)*5;
     progtime=15;
     uk=5;
     timesteps=(i+2)*5+progtime+5;
     tic
-    [co1{i,res},co2{i,res}]=rainprog(res*100,timesteps,prog,progtime,uk,'E:/Rainprog/m4t_BKM_wrx00_l2_dbz_v00_20130511170000.nc');
+    [co1{i,res/100},co2{i,res/100}]=rainprog(res,timesteps,prog,progtime,uk,'E:/Rainprog/m4t_BKM_wrx00_l2_dbz_v00_20130511160000.nc');
     toc
     end
 end
 
 colors=hsv(length(co1));
+
 figure
-for i=1:length(co1)
-plot(co1{i,1},'Color',colors(i,:))
+plot(0.5,'k')
 hold on
+plot(0.5,'k-.')
+legend('Auflösung 100m','Auflösung 200m')
+for i=1:length(co1)
+plot(co1{i,1},'Color',colors(i,:),'LineWidth',2)
 end
 for i=1:length(co1)
-plot(co1{i,2},'Color',colors(i,:),'Linestyle','-.')
-hold on
+plot(co1{i,2},'Color',colors(i,:),'Linestyle','-.','LineWidth',2)
 end
