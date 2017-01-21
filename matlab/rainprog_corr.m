a2dfc37099749d64cc2d8e24257a89e0c492e2f4
Michel Simon <u300675@thunder7.zmaw.de>
2017-01-21 20:29:57 +0100

    added code to find prognosises with a correlation of 0.8 in the first time step

--- matlab/rainprog_corr.m
+++ matlab/rainprog_corr.m
@@ -2,17 +2,19 @@ close all; clear;
 
 path=strcat('/home/zmaw/u300675/pattern_data/');
 files=dir(path);
-files=files(3:end);
+files=files(3:11);
 len=length(files);
 
-parfor j=1:len
-    for i=1:13
+for j=1:len
+    parfor i=1:13
         res=200;
         prog=(i+2)*5;
         progtime=30;
         uk=5;
         timesteps=prog+progtime+5;
         [co1{j,i},co2{j,i}]=rainprog(res,timesteps,prog,progtime,uk,strcat(path,files(j).name));
+        uk=7;
+        [co1_7{j,i},co2_7{j,i}]=rainprog(res,timesteps,prog,progtime,uk,strcat(path,files(j).name));
     end
     display(sprintf('Progress %d%%',floor(j/len*100)));
 end
@@ -22,9 +24,30 @@ colors=winter(size(co1,1));
 figure
 plot(0.5,'k')
 hold on
+k=1;
 for j=1:size(co1,1)
     for i=1:size(co1,1)
-        plot(co2{j,i},'Color',colors(j,:),'LineWidth',2)
+        plot(co1{j,i},'Color',colors(j,:),'LineWidth',2)
+        if co1{j,i}(1)<0.7
+            corr_low(k,1)=j;
+            corr_low(k,2)=i;
+            k=k+1;
+        end
     end
 end
 
+
+figure
+plot(0.5,'k')
+hold on
+k=1;
+for j=1:size(co1,1)
+    for i=1:size(co1,1)
+        plot(co1_7{j,i},'Color',colors(j,:),'LineWidth',2)
+        if co1_7{j,i}(1)<0.7
+            corr_7_low(k,1)=j;
+            corr_7_low(k,2)=i;
+            k=k+1;
+        end
+    end
+end
