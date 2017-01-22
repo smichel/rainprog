f9597aedab78cfad522ae44fc292149dd319f4cb
Michel Simon <u300675@thunder7.zmaw.de>
2017-01-22 15:52:05 +0100

    calculated more correlation matrices with different resolutions

--- matlab/rainprog_corr.m
+++ matlab/rainprog_corr.m
@@ -1,35 +1,42 @@
 close all; clear;
 
-path=strcat('F:/pattern_data/');
+path=strcat('/home/zmaw/u300675/pattern_data/');
 files=dir(path);
-files=files(3:7);
+files=files(3:end);
 len=length(files);
 
-for j=1:len
-    parfor i=1:13
+parfor j=1:len
+    for i=1:13
+        try
         res=200;
         prog=(i+2)*5;
         progtime=30;
-        uk=3;
+        uk=5;
         timesteps=prog+progtime+5;
         [co1{j,i},co2{j,i}]=rainprog(res,timesteps,prog,progtime,uk,strcat(path,files(j).name));
-        uk=10;
+        res=100;
         [co1_7{j,i},co2_7{j,i}]=rainprog(res,timesteps,prog,progtime,uk,strcat(path,files(j).name));
+        catch
+            co1{j,i}=NaN(1,progtime-1);
+            co2{j,i}=NaN(1,progtime-1);
+            co1_7{j,i}=NaN(1,progtime-1);
+            co2_7{j,i}=NaN(1,progtime-1);
+        end
     end
-    display(sprintf('Progress %d%%',floor(j/len*100)));
+    %display(sprintf('Progress %d%%',floor(j/len*100)));
 end
 
 colors=winter(size(co1,1));
 
 
 
-figure
-plot(0.5,'k')
-hold on
+% figure
+% plot(0.5,'k')
+% hold on
 k=1;
 for j=1:size(co1,1)
     for i=1:size(co1,2)
-        plot(co1{j,i},'Color',colors(j,:),'LineWidth',2)
+        %plot(co1{j,i},'Color',colors(j,:),'LineWidth',2)
         for k=1:size(co1{j,i},2)
             cor1(j,i,k)=co1{j,i}(k);
         end
@@ -41,15 +48,15 @@ for j=1:size(co1,1)
     end
 end
 
-plot(squeeze(mean(mean(cor1(:,:,:),2,'omitnan'),'omitnan')),'Color',[0 0 0],'LineWidth',5)
+%plot(squeeze(mean(mean(cor1(:,:,:),2,'omitnan'),'omitnan')),'Color',[0 0 0],'LineWidth',5)
 
-figure
-plot(0.5,'k')
-hold on
+% figure
+% plot(0.5,'k')
+% hold on
 k=1;
 for j=1:size(co1,1)
     for i=1:size(co1,2)
-        plot(co1_7{j,i},'Color',colors(j,:),'LineWidth',2)
+%         plot(co1_7{j,i},'Color',colors(j,:),'LineWidth',2)
         for k=1:size(co1_7{j,i},2)
             cor1_7(j,i,k)=co1_7{j,i}(k);
         end
@@ -60,7 +67,7 @@ for j=1:size(co1,1)
         end
     end
 end
-plot(squeeze(mean(mean(cor1_7(:,:,:),2,'omitnan'),'omitnan')),'Color',[0 0 0],'LineWidth',5)
-
-figure
-plot(squeeze(mean(mean(cor1_7(:,:,:),2,'omitnan'),'omitnan'))-squeeze(mean(mean(cor1(:,:,:),2,'omitnan'),'omitnan')))
+% plot(squeeze(mean(mean(cor1_7(:,:,:),2,'omitnan'),'omitnan')),'Color',[0 0 0],'LineWidth',5)
+save('correlation_factors6.mat');
+% figure
+% plot(squeeze(mean(mean(cor1_7(:,:,:),2,'omitnan'),'omitnan'))-squeeze(mean(mean(cor1(:,:,:),2,'omitnan'),'omitnan')))
