339a584cc37bad27bcfe4438fc49d2d0163117de
Michel Simon <u300675@thunder7.cen.uni-hamburg.de>
2017-06-28 18:45:25 +0200

    rainprog livetest failed for now it seems that the live data is bugged or wrong not reflectivity but rainrate will have to check this in future rest should work still need a filter which can see if rain is in the radar field and not scatter

--- matlab/live_test.m
+++ matlab/live_test.m
@@ -9,6 +9,8 @@ rain_threshold=0.1;
 num_maxes=6; %maximum numbers of points for the correlation boxes
 fileInfo=dir(name);
 timeStamp=fileInfo.date;
+data=dlmread(name,',',5,0);
+parpool(4)
 while true
     n_fileInfo=dir(name);
     n_timeStamp=n_fileInfo.date;
@@ -16,6 +18,7 @@ while true
         data=cat(3, dlmread(name,',',5,0),data);
         fileInfo=dir(name);
         timeStamp=fileInfo.date;
+        display(size(data,3))
         if size(data,3)==datalen
             display('Created a composit of 7 timesteps');
             tic
@@ -26,7 +29,7 @@ while true
             dimidrow=netcdf.defDim(ncid,'rows',size(prog_data,1));
             dimidcol=netcdf.defDim(ncid,'clo',size(prog_data,2));
             dimidtime=netcdf.defDim(ncid,'time',size(prog_data,3));
-            varid=netcdf.defVar(ncid,'prog_data','NC_DOUBLE',[dimidrow dimidcol dimidtime]);
+            varid=netcdf.defVar(ncid,timeStamp,'NC_DOUBLE',[dimidrow dimidcol dimidtime]);
             netcdf.endDef(ncid);
             netcdf.putVar(ncid,varid,prog_data);
             netcdf.close(ncid);
@@ -39,4 +42,18 @@ while true
         data(:,:,end)=[];
     end
     pause(1)
+end
+figure
+for i=8:-1:1
+    imagesc(data(:,:,i),[0 100]);
+    colorbar
+        drawnow
+        frame = getframe(1);
+        im = frame2im(frame);
+        [imind,cm] = rgb2ind(im,256);
+        if i == 8
+            imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
+        else
+            imwrite(imind,cm,filename,'gif','WriteMode','append');
+        end
 end
